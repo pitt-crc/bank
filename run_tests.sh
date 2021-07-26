4949a1b394141aabcef677c316215b142e961600
Daniel Perrefort <djperrefort@pitt.edu>
2021-07-26 14:04:36 -0400

    Moves test suite cleanup to end of tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-rm test.db proposal.json investor.json proposal_archive.json investor_archive.json
-
-if [ $CRC_TEST = 'true' ]; then
-    sudo sacctmgr -i modify account where account=sam cluster=smp,gpu,mpi,htc set rawusage=0
+if [ "$CRC_TEST" = 'true' ]; then
+    #sudo sacctmgr -i modify account where account=sam cluster=smp,gpu,mpi,htc set rawusage=0
     for bat in $(ls tests/*.bats); do
         echo "====== BEGIN $bat ======"
         bats $bat
@@ -13,6 +11,9 @@ if [ $CRC_TEST = 'true' ]; then
         echo "======  END $bat  ======"
     done
 else
-    echo "CRC_TEST must be set to `true` in working env to run tests."
+    echo "CRC_TEST must be set to 'true' in working env to run tests."
     echo "This is to protect accidental overwrite of the operational database."
 fi
+
+# Clean up after test suite
+rm test.db proposal.json investor.json proposal_archive.json investor_archive.json 2> /dev/null
