a1cdbba9008406bd72f6cb44375dc84ff3319cef
Daniel Perrefort <djperrefort@pitt.edu>
2021-08-16 11:18:52 -0400

    Replaces dataset ORM with SQLAlchemy

--- bank/orm.py
+++ bank/orm.py
@@ -1,9 +1,69 @@
-import dataset
+from sqlalchemy import Column, Date, Integer, Text
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import create_database, database_exists
 
-from settings import app_settings
+from .settings import app_settings
 
-db = dataset.connect(f'sqlite:///{app_settings.db_test_path if app_settings.is_testing else app_settings.db_path}')
-proposal_table = db["proposal"]
-investor_table = db["investor"]
-investor_archive_table = db["investor_archive"]
-proposal_archive_table = db["proposal_archive"]
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class Investor(Base):
+    __tablename__ = 'investor'
+
+    id = Column(Integer, primary_key=True)
+    account = Column(Text)
+    start_date = Column(Date)
+    end_date = Column(Date)
+    proposal_type = Column(Integer)
+    service_units = Column(Integer)
+    current_sus = Column(Integer)
+    withdrawn_sus = Column(Integer)
+    rollover_sus = Column(Integer)
+
+
+class InvestorArchive(Base):
+    __tablename__ = 'investor_archive'
+
+    id = Column(Integer, primary_key=True)
+    account = Column(Text)
+    start_date = Column(Date)
+    end_date = Column(Date)
+    exhaustion_date = Column(Date)
+    service_units = Column(Integer)
+    current_sus = Column(Integer)
+    proposal_id = Column(Integer)
+    investor_id = Column(Integer)
+
+
+class Proposal(Base):
+    __tablename__ = 'proposal'
+
+    id = Column(Integer, primary_key=True)
+    account = Column(Text)
+    start_date = Column(Date)
+    end_date = Column(Date)
+    percent_notified = Column(Integer)
+    proposal_type = Column(Integer)
+
+
+class ProposalArchive(Base):
+    __tablename__ = 'proposal_archive'
+
+    id = Column(Integer, primary_key=True)
+    account = Column(Text)
+    start_date = Column(Date)
+    end_date = Column(Date)
+
+
+# Dynamically add columns for each of the managed clusters
+for cluster in app_settings.clusters:
+    setattr(Proposal, cluster, Column(Integer))
+    setattr(ProposalArchive, cluster, Column(Integer))
+    setattr(ProposalArchive, cluster + '_usage', Column(Integer))
+
+engine = create_engine(app_settings.db_path)
+if not database_exists(engine.url):
+    create_database(engine.url)
+    metadata.create_all(engine)
