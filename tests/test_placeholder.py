1c26159503513721da56302d8c5502142a5fd7ef
Daniel Perrefort <djperrefort@pitt.edu>
2021-08-16 08:06:25 -0400

    Modify CI workflow to run python test suite (#28)
    * Renames tests -> bash_tests
    
    * Adds placeholder python test suite
    
    * Adds workflow for running python tests
    
    * Comments out bash tests
    
    * Removes package install step for python testing ci
    
    * Adds missing init file to tests directory

---
+++ tests/test_placeholder.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+# A place holder test so there is at least one test case for the CI to run
+# this will be replaced with real tests later on
+class DummyTest(TestCase):
+
+    def runTest(self) -> None:
+        pass
