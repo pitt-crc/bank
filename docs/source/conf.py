37c79a32169a05181f5b6100da8f5fc64ee4cc3f
Daniel Perrefort <djperrefort@pitt.edu>
2021-08-16 08:19:21 -0400

    Configures sphinx extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,6 +28,14 @@ author = 'Pitt CRC'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    'sphinx.ext.napoleon',  # Napoleon need to be loaded BEFORE sphinx_autodoc_typehints
+    'sphinx_copybutton',
+    'sphinx_autodoc_typehints',
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,7 +52,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
