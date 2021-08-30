e41ca961ee332142914b843e0f5ac77abbe38f85
Daniel Perrefort <djperrefort@pitt.edu>
2021-08-30 11:43:48 -0400

    Outlines desired tests for ExportMixin

--- tests/orm/mixins/test_ExportMixin.py
+++ tests/orm/mixins/test_ExportMixin.py
@@ -7,4 +7,20 @@ DummyTable = create_table_with_mixin(ExportMixin)
 
 
 class ExportingToJson(TestCase):
-    ...
+    """Test the exporting of row data to json format"""
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        pass
+
+    def test_date_format_matches_settings(self) -> None:
+        self.fail()
+
+    def test_enum_cast_to_int(self) -> None:
+        self.fail()
+
+    def test_return_is_json_parsable(self) -> None:
+        self.fail()
+
+    def test_includes_all_columns(self) -> None:
+        self.fail()
