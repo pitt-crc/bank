3107a7051c3808575219dcfc84c4ac37c672bddd
Daniel Perrefort <djperrefort@pitt.edu>
2021-08-30 11:12:45 -0400

    Extends tests for DictAccessPatternMixin

--- tests/orm/mixins/test_DictAccessPatternMixin.py
+++ tests/orm/mixins/test_DictAccessPatternMixin.py
@@ -16,7 +16,7 @@ class DummyTable(Base, DictAccessPatternMixin):
 
 
 class TestClassIsIterable(TestCase):
-    """Tests tables with the ``CustomBase`` mixin are iterable"""
+    """Test subclassed tables are iterable"""
 
     def test_is_iterable(self) -> None:
         """Test instances can be cast to an iterator"""
@@ -31,3 +31,30 @@ class TestClassIsIterable(TestCase):
 
         self.assertEqual(dummy_obj.int_col, obj_as_dict['int_col'])
         self.assertEqual(dummy_obj.str_col, obj_as_dict['str_col'])
+
+
+class TestKeyIndexing(TestCase):
+    """Test subclassed tables support key based indexing"""
+
+    def test_get_item(self) -> None:
+        table_row = DummyTable(int_col=1, str_col='a')
+        self.assertEqual(table_row.int_col, table_row['int_col'])
+        self.assertEqual(table_row.str_col, table_row['str_col'])
+
+    def test_set_item(self) -> None:
+        table_row = DummyTable(int_col=1, str_col='a')
+        table_row['str_col'] = 'b'
+        self.assertEqual('b', table_row.str_col)
+        self.assertEqual('b', table_row['str_col'])
+
+
+class TestUpdate(TestCase):
+    """Tests for the ``update`` method"""
+
+    def test_row_is_updated(self) -> None:
+        table_row = DummyTable(int_col=1, str_col='a')
+        new_values = dict(int_col=2, str_col='b')
+
+        table_row.update(**new_values)
+        self.assertEqual(new_values['int_col'], table_row.int_col)
+        self.assertEqual(new_values['str_col'], table_row.str_col)
