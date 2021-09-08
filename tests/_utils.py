87a7596d42c18f2e51e509d678428f5cb9e75374
Daniel Perrefort <djperrefort@pitt.edu>
2021-09-08 11:02:24 -0400

    Adds testing base class for protecting underlying db

--- tests/_utils.py
+++ tests/_utils.py
@@ -1,6 +1,25 @@
 """Generic utilities used by the test suite"""
 
 import os
+from unittest import TestCase
+
+from bank.orm import engine
+
+
+class DatabaseSafeTest(TestCase):
+    """Base class for the UnitTest framework that protects database transactions"""
+
+    def setUp(self) -> None:
+        """Set up a database transaction to be rolled back after tests"""
+
+        self.__transaction = engine.connect().begin()
+        super(DatabaseSafeTest, self).setUp()
+
+    def tearDown(self) -> None:
+        """Rollback database changes made during testing"""
+
+        self.__transaction.rollback()
+        super(DatabaseSafeTest, self).tearDown()
 
 
 class CleanEnviron:
