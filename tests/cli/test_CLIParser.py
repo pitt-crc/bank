6bfd43181d3717e1a79da7e63e31c3b20dc5b1e8
Daniel Perrefort <djperrefort@pitt.edu>
2021-09-09 14:25:56 -0400

    Adds basic CLI parser tests

---
+++ tests/cli/test_CLIParser.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+
+from bank.cli import CLIParser
+
+
+class SubparserNames(TestCase):
+    """Test the names of various subparsers can be parsed"""
+
+    test_names = [
+        'insert', 'modify', 'add', 'change', 'renewal', 'date',
+        'date_investment', 'investor', 'withdraw', 'info', 'usage',
+        'check_sus_limit', 'check_proposal_end_date', 'check_proposal_violations',
+        'get_sus', 'dump', 'import_proposal', 'import_investor', 'release_hold',
+        'alloc_sus', 'reset_raw_usage', 'find_unlocked', 'lock_with_notification'
+    ]
+
+    def runTest(self) -> None:
+        """Check each subparser name parses without error"""
+
+        for subparser_name in self.test_names:
+            try:
+                CLIParser().parse_args([subparser_name])
+
+            except:
+                self.fail(f'Could not parse name {subparser_name}')
