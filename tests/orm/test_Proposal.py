54a30fb25310a67a5362d7dc1e039d2effd06d67
Daniel Perrefort <djperrefort@pitt.edu>
2021-09-21 12:49:17 -0400

    Full test coverage for Proposal class

--- tests/orm/test_Proposal.py
+++ tests/orm/test_Proposal.py
@@ -1,10 +1,13 @@
+"""Tests for the ``Proposal`` class"""
+
+from datetime import date, timedelta
 from unittest import TestCase
 
 from bank.orm import Proposal
 from bank.settings import app_settings
 
 
-class TestHasDynamicColumns(TestCase):
+class HasDynamicColumns(TestCase):
     """Test for dynamically added columns based on administered cluster names"""
 
     def runTest(self) -> None:
@@ -16,11 +19,11 @@ class TestHasDynamicColumns(TestCase):
                 self.fail(f'Table {Proposal.__tablename__} has no column {cluster}')
 
 
-class TestColumnValueValidation(TestCase):
-    """Tests for the validation of values assigned to attributes vis setters"""
+class PercentNotifiedValidation(TestCase):
+    """Tests for the validation of the ``percent_notified``` column"""
 
-    def test_percent_notified_validation(self) -> None:
-        """Test for ValueError when percent_notified is not between 0 and 100"""
+    def test_percent_notified_out_of_range(self) -> None:
+        """Test for a ``ValueError`` when ``percent_notified`` is not between 0 and 100"""
 
         with self.assertRaises(ValueError):
             Proposal(percent_notified=-1)
@@ -31,8 +34,58 @@ class TestColumnValueValidation(TestCase):
         Proposal(percent_notified=0)
         Proposal(percent_notified=100)
 
-    def test_percent_notified_setter(self) -> None:
-        """Test for ValueError when percent_notified is not between 0 and 100"""
+    def test_percent_notified_in_range(self) -> None:
+        """Test no error is raised when ``percent_notified`` is not between 0 and 100"""
 
         proposal = Proposal(percent_notified=50)
         self.assertEqual(50, proposal.percent_notified)
+
+
+class ServiceUnitsValidation(TestCase):
+    """Tests for the validation of the service units"""
+
+    def test_negative_service_units(self) -> None:
+        """Test for a ``ValueError`` when the number of service units are negative"""
+
+        for cluster in app_settings.clusters:
+            with self.assertRaises(ValueError):
+                Proposal(**{cluster: -1})
+
+    def test_positive_service_units(self) -> None:
+        """Test no error is raised when the number of service units are positive"""
+
+        for cluster in app_settings.clusters:
+            proposal = Proposal(**{cluster: 10})
+            self.assertEqual(10, getattr(proposal, cluster))
+
+
+class TOArchiveObject(TestCase):
+    """Test the conversion of a proposal to an archive object"""
+
+    def setUp(self) -> None:
+        """Create a proposal instance for testing"""
+
+        self.proposal = Proposal(
+            account_name='username',
+            start_date=date.today(),
+            end_date=date.today() + timedelta(days=1),
+            percent_notified=10,
+            proposal_type=1
+        )
+
+        for i, cluster in enumerate(app_settings.clusters):
+            setattr(self.proposal, cluster, i)
+
+    def test_correct_column_values(self) -> None:
+        """Test the attributes of the returned object match the original proposal"""
+
+        # We could do something clever here with sets and dictionaries
+        # Instead lets keep it simple and just check each column one at a time
+        archive_obj = self.proposal.to_archive_object()
+        self.assertEqual(self.proposal.id, archive_obj.id)
+        self.assertEqual(self.proposal.account_name, archive_obj.account_name)
+        self.assertEqual(self.proposal.start_date, archive_obj.start_date)
+        self.assertEqual(self.proposal.end_date, archive_obj.end_date)
+        self.assertEqual(self.proposal.proposal_type, archive_obj.proposal_type)
+        for cluster in app_settings.clusters:
+            self.assertEqual(getattr(self.proposal, cluster), getattr(archive_obj, cluster))
