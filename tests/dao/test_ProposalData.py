66f07c74ae521f834fb213bc0cebb88fbae675ea
Daniel Perrefort <djperrefort@gmail.com>
2021-10-25 14:13:04 -0400

    Moves setup tasks into dedicated module [skip ci]

--- tests/dao/test_ProposalData.py
+++ tests/dao/test_ProposalData.py
@@ -1,24 +1,10 @@
 from unittest import TestCase
-from unittest.mock import patch
 
 from bank.dao import ProposalData
 from bank.exceptions import MissingProposalError, ProposalExistsError
 from bank.orm import Session, Proposal
 from bank.settings import app_settings
-
-
-class GenericSetup:
-    """Reusable setup mixin for configuring a unittest class"""
-
-    def setUp(self) -> None:
-        """Delete any proposals that may already exist for the test account"""
-
-        with Session() as session:
-            session.query(Proposal).filter(Proposal.account_name == app_settings.test_account).delete()
-            session.commit()
-
-        self.account = ProposalData(app_settings.test_account)
-        self.account.create_proposal()
+from tests.dao.utils import GenericSetup
 
 
 class CreateProposal(TestCase):
