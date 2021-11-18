67c48bca40dde6228def1e8440cb36da0bf5c422
Daniel Perrefort <djperrefort@gmail.com>
2021-11-18 10:13:06 -0500

    Outlines tests for 'investor_modify' subparser

--- tests/cli/test_CLIParser.py
+++ tests/cli/test_CLIParser.py
@@ -239,18 +239,48 @@ class Investor(ProposalSetup, TestCase):
             CLIParser().execute(['investor', app_settings.test_account, '1000'])
 
 
-class InvestorModify(TestCase):
+class InvestorModify(InvestorSetup, TestCase):
     """Tests for the ``investor_modify`` subparser"""
 
     def test_modify_updates_SUs(self) -> None:
         """Test the command updates sus on the given investment"""
 
         account = dao.Account(app_settings.test_account)
-        inv_id, original_sus = next(iter(account.get_investment_sus().items()))
+        old_inv = account.get_investment_info()[0]
 
-        new_sus = original_sus + 10
-        CLIParser().execute(['investor_modify', inv_id, new_sus])
-        self.assertEqual(new_sus, next(iter(account.get_investment_sus().values())))
+        new_sus = old_inv['service_units'] + 10
+        CLIParser().execute(['investor_modify', str(old_inv['id']), str(new_sus)])
+
+        new_inv = account.get_investment_info()[0]
+        self.assertEqual(new_sus, new_inv['service_units'])
+
+    def test_error_on_missing_proposal(self) -> None:
+        """Test an error is raised when passed an account with a missing proposal"""
+
+        with Session() as session:
+            session.query(Proposal).filter(Proposal.account_name == app_settings.test_account).delete()
+            session.commit()
+
+        account = dao.Account(app_settings.test_account)
+        inv = account.get_investment_info()[0]
+        with self.assertRaises(MissingProposalError):
+            CLIParser().execute(['investor_modify', str(inv['id']), '10_000'])
+
+    def test_error_on_missing_investment(self) -> None:
+        """Test an error is raised when passed an invalid investment id"""
+
+        with self.assertRaises(MissingProposalError):
+            CLIParser().execute(['investor_modify', '123', '10_000'])
+
+    def test_date_is_updated(self) -> None:
+        """Test the command updates the date on the investment"""
+
+        account = dao.Account(app_settings.test_account)
+        inv = account.get_investment_info()[0]
+        new_date = (inv['start_date'] - timedelta(days=1)).strftime(app_settings.date_format)
+
+        CLIParser().execute(['investor_modify', str(inv['id']), f'--date={new_date}'])
+        self.assertEqual(new_date, account.get_proposal_info()['start_date'])
 
 
 class Renewal(TestCase):
