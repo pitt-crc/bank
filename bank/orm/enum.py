777ab436296567ef30590108fd86424076a28221
Daniel Perrefort <djperrefort@pitt.edu>
2021-11-22 14:50:36 -0500

    Adds descriptive error message for invalid proposal types (#108)
    * Adds descriptive error message for invalid proposal types
    
    * Updates expected error type in test suite

--- bank/orm/enum.py
+++ bank/orm/enum.py
@@ -9,3 +9,17 @@ class ProposalType(Enum):
     PROPOSAL = 0
     CLASS = 1
     INVESTOR = 2
+
+    @classmethod
+    def get(cls, name):
+        """Return the enum instance that is equivalent to the non-enumerated value
+
+        Args:
+            The name of the value to return an enumerated instance for
+        """
+
+        try:
+            return cls[name]
+
+        except KeyError:
+            cls(name)
