a1d2f3e83a8fa6a036aeb17afad42e3e75e73162
Daniel <djperrefort@gmail.com>
2021-12-02 14:47:06 -0500

    Uses seperate classes for seperate subparsers

--- bank/cli.py
+++ bank/cli.py
@@ -36,115 +36,138 @@ _account = dict(dest='--account', required=True, help='The slurm account to admi
 _user = dict(dest='--user', nargs='?', help='Optionally create a user under the parent slurm account')
 _notify = dict(dest='--notify', action='store_true', help='Optionally notify the account holder via email')
 _ptype = dict(dest='--ptype', default='proposal', options=['proposal', 'class'], help='The proposal type')
-_date = dict(dest='--date', nargs='?', type=lambda s: datetime.strptime(s, app_settings.date_format))
+_date = dict(dest='--date', nargs='?', type=lambda s: datetime.strptime(s, settings.date_format))
 _sus = dict(dest='--sus', type=int, help='The number of SUs you want to insert')
 _inv_id = dict(dest='--id', type=int, help='The investment proposal id')
 
 
-class CLIParser(ArgumentParser):
+class BaseParser(ArgumentParser):
+
     def __init__(self) -> None:
-        """Parser for command line arguments
-
-        Parser arguments are broken down by the service being administered, the
-        action being taken, and any arguments needed to evaluate that action:
-
-        ``my_cli.py service action --options``
-
-        For a complete usage description, use ``CLIParser().print_usage()``.
-        """
-
         super().__init__()
-        service_subparsers = self.add_subparsers(parser_class=ArgumentParser)
+        self.service_subparsers = self.add_subparsers(parser_class=ArgumentParser)
 
-        info = service_subparsers.add_parser('info', help='Print usage and allocation information')
+
+class AdminParser(BaseParser):
+
+    def __init__(self) -> None:
+        super().__init__()
+        admin_parser = self.service_subparsers.add_parser('admin')
+        admin_subparsers = admin_parser.add_subparsers()
+
+        info = admin_subparsers.add_parser('info', help='Print usage and allocation information')
         info.add_argument('account', help='The account to print information for')
 
-        notify = service_subparsers.add_parser('notify', help='Send any pending email notifications')
+        notify = admin_subparsers.add_parser('notify', help='Send any pending email notifications')
         notify.add_argument('account', help='The account to process notification for')
 
-        # Parsers for the Slurm service
-        slurm_parser = service_subparsers.add_parser('slurm', help='Administrative tools for slurm accounts')
+
+class SlurmParser(BaseParser):
+
+    def __init__(self) -> None:
+        super().__init__()
+        slurm_parser = self.service_subparsers.add_parser('slurm', help='Administrative tools for slurm accounts')
         slurm_subparsers = slurm_parser.add_subparsers(title="Slurm actions")
 
         slurm_create = slurm_subparsers.add_parser('create', help='Create a new slurm account')
-        self._add_args_to_parser(slurm_create, _account, _user)
+        slurm_create.add_argument(**_account)
+        slurm_create.add_argument(**_user)
 
         slurm_delete = slurm_subparsers.add_parser('delete', help='Delete an existing slurm account')
-        self._add_args_to_parser(slurm_delete, _account, _user)
+        slurm_delete.add_argument(**_account)
+        slurm_delete.add_argument(**_user)
 
         slurm_lock = slurm_subparsers.add_parser('lock', help='Lock a slurm account from submitting any jobs')
-        self._add_args_to_parser(slurm_lock, _account, _notify)
+        slurm_lock.add_argument(**_account)
+        slurm_lock.add_argument(**_notify)
 
         slurm_unlock = slurm_subparsers.add_parser('unlock', help='Allow a slurm account to submit jobs')
-        self._add_args_to_parser(slurm_unlock, _account, _notify)
+        slurm_unlock.add_argument(**_account)
+        slurm_unlock.add_argument(**_notify)
 
-        # Parsers for user proposals
-        proposal_parser = service_subparsers.add_parser('proposal', help='Administrative tools for user proposals')
+
+class ProposalParser(BaseParser):
+
+    def __init__(self) -> None:
+        super().__init__()
+        proposal_parser = self.service_subparsers.add_parser('proposal', help='Administrative tools for user proposals')
         proposal_subparsers = proposal_parser.add_subparsers(title="Proposal actions")
 
-        proposal_create = proposal_subparsers.add_parser('create',
-                                                         help='Create a new proposal for an existing slurm account')
-        self._add_args_to_parser(proposal_create, _account, _ptype, include_clusters=True)
+        proposal_create = proposal_subparsers.add_parser('create', help='Create a new proposal for an existing slurm account')
+        proposal_create.add_argument(**_account)
+        proposal_create.add_argument(**_ptype)
+        self._add_cluster_args(proposal_create)
 
         proposal_delete = proposal_subparsers.add_parser('delete', help='Delete an existing account proposal')
-        self._add_args_to_parser(proposal_delete, _account, include_clusters=True)
+        proposal_delete.add_argument(**_account)
+        self._add_cluster_args(proposal_delete)
 
         proposal_add = proposal_subparsers.add_parser('add', help='Add service units to an existing proposal')
-        self._add_args_to_parser(proposal_add, _account, include_clusters=True)
+        proposal_add.add_argument(**_account)
+        self._add_cluster_args(proposal_add)
 
-        proposal_subtract = proposal_subparsers.add_parser('subtract',
-                                                           help='Subtract service units from an existing proposal')
-        self._add_args_to_parser(proposal_subtract, _account, include_clusters=True)
+        proposal_subtract = proposal_subparsers.add_parser('subtract', help='Subtract service units from an existing proposal')
+        proposal_subtract.add_argument(**_account)
+        self._add_cluster_args(proposal_subtract)
 
-        proposal_overwrite = proposal_subparsers.add_parser('overwrite',
-                                                            help='Overwrite properties of an existing proposal')
-        self._add_args_to_parser(proposal_overwrite, _account, _date, include_clusters=True)
-
-        # Parsers for investments
-        investment_parser = service_subparsers.add_parser('investment',
-                                                          help='Administrative tools for user investments')
-        investment_subparsers = investment_parser.add_subparsers(title="Investment actions")
-
-        investment_create = investment_subparsers.add_parser('create', description='Create a new investment')
-        self._add_args_to_parser(investment_create, _account, _sus)
-
-        investment_delete = investment_subparsers.add_parser('delete', description='Delete an existing investment')
-        self._add_args_to_parser(investment_delete, _account, _inv_id, _sus)
-
-        investment_add = investment_subparsers.add_parser('add', help='Add service units to an existing investment')
-        self._add_args_to_parser(investment_add, _account, _inv_id, _sus)
-
-        investment_subtract = investment_subparsers.add_parser('subtract',
-                                                               help='Subtract service units from an existing investment')
-        self._add_args_to_parser(investment_subtract, _account, _inv_id, _sus)
-
-        investment_overwrite = investment_subparsers.add_parser('overwrite',
-                                                                help='Overwrite properties of an existing investment')
-        self._add_args_to_parser(investment_overwrite, _account, _inv_id, _sus, _date)
-
-        investment_advance = investment_subparsers.add_parser('advance')
-        self._add_args_to_parser(investment_advance, _account, _sus)
-
-        investment_renew = investment_subparsers.add_parser('renew')
-        self._add_args_to_parser(investment_renew, _account)
+        proposal_overwrite = proposal_subparsers.add_parser('overwrite', help='Overwrite properties of an existing proposal')
+        proposal_overwrite.add_argument(**_account)
+        proposal_overwrite.add_argument(**_date)
+        self._add_cluster_args(proposal_overwrite)
 
     @staticmethod
-    def _add_args_to_parser(parser: ArgumentParser, *arg_definitions: dict, include_clusters: bool = False) -> None:
+    def _add_cluster_args(parser: ArgumentParser) -> None:
         """Add argument definitions to the given command line subparser
 
         Args:
             parser: The parser to add arguments to
-            *arg_definitions: Dictionary with arguments for ``parser.add_argument``
-            include_clusters: Dynamically add arguments from application settings for the service units of each cluster
         """
 
-        for arg_def in arg_definitions:
-            arg_def = arg_def.copy()
-            parser.add_argument(**arg_def)
+        for cluster in settings.clusters:
+            parser.add_argument(f'--{cluster}', type=int, help=f'The {cluster} limit in CPU Hours', default=0)
 
-        if include_clusters:
-            for cluster in settings.clusters:
-                parser.add_argument(f'--{cluster}', type=int, help=f'The {cluster} limit in CPU Hours', default=0)
+
+class InvestmentParser(BaseParser):
+
+    def __init__(self) -> None:
+        super().__init__()
+        investment_parser = self.service_subparsers.add_parser('investment', help='Administrative tools for user investments')
+        investment_subparsers = investment_parser.add_subparsers(title="Investment actions")
+
+        investment_create = investment_subparsers.add_parser('create', description='Create a new investment')
+        investment_create.add_argument(**_account)
+        investment_create.add_argument(**_sus)
+
+        investment_delete = investment_subparsers.add_parser('delete', description='Delete an existing investment')
+        investment_delete.add_argument(**_account)
+        investment_delete.add_argument(**_inv_id)
+        investment_delete.add_argument(**_sus)
+
+        investment_add = investment_subparsers.add_parser('add', help='Add service units to an existing investment')
+        investment_add.add_argument(**_account)
+        investment_add.add_argument(**_inv_id)
+        investment_delete.add_argument(**_sus)
+
+        investment_subtract = investment_subparsers.add_parser('subtract', help='Subtract service units from an existing investment')
+        investment_subtract.add_argument(**_account)
+        investment_subtract.add_argument(**_inv_id)
+        investment_delete.add_argument(**_sus)
+
+        investment_overwrite = investment_subparsers.add_parser('overwrite', help='Overwrite properties of an existing investment')
+        investment_overwrite.add_argument(**_account)
+        investment_overwrite.add_argument(**_inv_id)
+        investment_delete.add_argument(**_sus)
+        investment_delete.add_argument(**_date)
+
+        investment_advance = investment_subparsers.add_parser('advance')
+        investment_advance.add_argument(**_account)
+        investment_advance.add_argument(**_sus)
+
+        investment_renew = investment_subparsers.add_parser('renew')
+        investment_renew.add_argument(**_account)
+
+
+class CLIParser(AdminParser, SlurmParser, ProposalParser, InvestmentParser):
 
     def error(self, message):
         """Print a usage message to stderr raise the message as an exception.
