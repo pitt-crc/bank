6dac5b8e4def8eb089f3d37d6972d3aa72f60062
Daniel <djperrefort@gmail.com>
2022-03-22 14:53:08 -0400

    Restructures ORM for new table structure

--- bank/orm/tables.py
+++ bank/orm/tables.py
@@ -6,25 +6,23 @@ API Reference
 
 from __future__ import annotations
 
-import logging
 from datetime import date
 from logging import getLogger
 
-from sqlalchemy import Column, Date, Integer, String, Enum
+from sqlalchemy import Column, Date, Integer, String, Enum, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import validates
+from sqlalchemy.orm import validates, relationship
 
 from .utils import Validators, ProposalEnum
 from .. import settings
-from ..system import SlurmAccount
 
 LOG = getLogger('bank.orm.tables')
 Base = declarative_base()
 metadata = Base.metadata
 
 
-class Proposal(Base):
-    """Class representation of the ``proposal`` table
+class Proposal(Base, Validators):
+    """Meta data for user proposals
 
     Table Fields:
       - id: Integer
@@ -43,9 +41,11 @@ class Proposal(Base):
     start_date = Column(Date, nullable=False)
     end_date = Column(Date, nullable=False)
     percent_notified = Column(Integer, nullable=False)
+    allocations = relationship('Allocation', backref='Allocation.proposal_id')
 
-    _validate_service_units = validates(*settings.clusters)(Validators.validate_service_units)
-    _validate_percent_notified = validates('percent_notified')(Validators.validate_percent_notified)
+    @validates('percent_notified')
+    def _validate_percent_notified(self, key: str, value: int) -> None:
+        super()._validate_percent_notified(key, value)
 
     @property
     def expired(self) -> bool:
@@ -53,57 +53,47 @@ class Proposal(Base):
 
         return self.end_date <= date.today()
 
-    @property
+    def allocated(self, cluster: str) -> int:
+        """Return the service units allocated under the proposal for a given cluster
+
+        Args:
+            cluster: The name of the cluster
+        """
+
+        raise NotImplementedError
+
     def total_allocated(self) -> int:
+        """Return the total service units allocated under the proposal"""
+
         return sum(getattr(self, c) for c in settings.clusters)
 
-    def to_archive_object(self) -> ProposalArchive:
-        """Return data from the current row as an ``InvestorArchive`` instance"""
 
-        logging.debug(f'Creating archive object for proposal {self.id}')
-        archive_obj = ProposalArchive(
-            id=self.id,
-            proposal_type=self.proposal_type,
-            account_name=self.account_name,
-            start_date=self.start_date,
-            end_date=self.end_date,
-        )
-
-        # Add the usage and allocation for each cluster to the archive object
-        slurm_acct = SlurmAccount(self.account_name)
-        for cluster in settings.clusters:
-            setattr(archive_obj, cluster, getattr(self, cluster))
-            setattr(archive_obj, f'{cluster}_usage', slurm_acct.get_cluster_usage(cluster))
-
-        return archive_obj
-
-
-class ProposalArchive(Base):
-    """Class representation of the ``proposal_archive`` table
+class Allocation(Base, Validators):
+    """Service unit allocations on individual clusters
 
     Table Fields:
       - id: Integer
-      - account_name: String
-      - proposal_type: ProposalEnum
-      - start_date: Date
-      - end_date: Date
-    """
+      - proposal_id: Integer
+      - cluster_name: String
+      - service_units: Integer
+      - final_usage: Integer
+      """
 
-    __tablename__ = 'proposal_archive'
+    __tablename__ = 'allocation'
 
     id = Column(Integer, primary_key=True)
-    account_name = Column(String, nullable=False)
-    proposal_type = Column(Enum(ProposalEnum), nullable=False)
-    start_date = Column(Date, nullable=False)
-    end_date = Column(Date, nullable=False)
+    proposal_id = Column(Integer, ForeignKey(Proposal.id), primary_key=True)
+    cluster_name = Column(String, nullable=False)
+    service_units = Column(Integer, nullable=False)
+    final_usage = Column(Integer, nullable=True)
 
-    _validate_service_units = validates(*settings.clusters, *(f'{c}_usage' for c in settings.clusters))(
-        Validators.validate_service_units
-    )
+    @validates('service_units')
+    def _validate_service_units(self, key: str, value: int) -> None:
+        super()._validate_service_units(key, value)
 
 
-class Investor(Base):
-    """Class representation of the ``investor`` table
+class Investor(Base, Validators):
+    """Service unit allocations granted in exchange for user investment
 
     Table Fields:
       - id: Integer
@@ -126,58 +116,14 @@ class Investor(Base):
     current_sus = Column(Integer, nullable=False)
     withdrawn_sus = Column(Integer, nullable=False)
     rollover_sus = Column(Integer, nullable=False)
+    exhaustion_date = Column(Date, nullable=True)
 
-    _validate_service_units = validates('service_units')(Validators.validate_service_units)
+    @validates('service_units')
+    def _validate_service_units(self, key: str, value: int) -> None:
+        super()._validate_service_units(key, value)
 
     @property
     def expired(self) -> bool:
-        """Return whether the investment is past its end date or is fully withdrawn with no remaining service units."""
+        """Return whether the investment is past its end date or is fully withdrawn with no remaining service units"""
 
         return (self.end_date <= date.today()) or (self.current_sus == 0 and self.withdrawn_sus >= self.service_units)
-
-    def to_archive_object(self) -> InvestorArchive:
-        """Return data from the current row as an ``InvestorArchive`` instance"""
-
-        logging.debug(f'Creating archive object for Investor {self.id}')
-        return InvestorArchive(
-            id=self.id,
-            account_name=self.account_name,
-            start_date=self.start_date,
-            end_date=self.end_date,
-            exhaustion_date=date.today(),
-            service_units=self.service_units,
-            current_sus=self.current_sus
-        )
-
-
-class InvestorArchive(Base):
-    """Class representation of the ``investor_archive`` table
-
-    Table Fields:
-      - id: Integer
-      - account_name: String
-      - start_date: Date
-      - end_date: Date
-      - exhaustion_date: Date
-      - service_units: Integer
-      - current_sus: Integer
-    """
-
-    __tablename__ = 'investor_archive'
-
-    id = Column(Integer, primary_key=True)
-    account_name = Column(String, nullable=False)
-    start_date = Column(Date, nullable=False)
-    end_date = Column(Date, nullable=False)
-    exhaustion_date = Column(Date, nullable=False)
-    service_units = Column(Integer, nullable=False)
-    current_sus = Column(Integer, nullable=False)
-
-    _validate_service_units = validates('service_units')(Validators.validate_service_units)
-
-
-# Dynamically add columns for each of the managed clusters
-for _cluster in settings.clusters:
-    setattr(Proposal, _cluster, Column(Integer, default=0))
-    setattr(ProposalArchive, _cluster, Column(Integer, nullable=False))
-    setattr(ProposalArchive, f'{_cluster}_usage', Column(Integer, nullable=False))
--- bank/orm/utils.py
+++ bank/orm/utils.py
@@ -1,34 +1,44 @@
-"""Extends the default functionality of SQLAlchemy tables
+"""General utilities and data types used for interacting with the application database.
 
 API Reference
 -------------
 """
 
+from __future__ import annotations
+
 import enum
 
 
 class ProposalEnum(enum.Enum):
+    """Enum type for different user proposals"""
+
     Unknown = 99
     Proposal = 1
     Class = 2
 
-    def __str__(self):
-        return self.name
-
     @classmethod
-    def from_string(cls, s):
+    def from_string(cls, string: str) -> ProposalEnum:
+        """Return an instance of the parent class corresponding to a proposal type"""
+
         try:
-            return cls[s]
+            return cls[string]
 
         except KeyError:
-            raise ValueError(f'Invalid ProposalEnum type: {s}')
+            raise ValueError(f'Invalid ProposalEnum type: {string}')
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Validators:
     """Methods for validating column values before interacting with the database"""
 
-    def validate_service_units(self, key: str, value: int) -> int:
+    def _validate_service_units(self, key: str, value: int) -> int:
         """Verify the given value is a non-negative integer
+
+        Args:
+            key: Name of the column data is being entered into
+            value: The value to test
 
         Raises:
             ValueError: If the given value does not match required criteria
@@ -39,8 +49,12 @@ class Validators:
 
         return value
 
-    def validate_percent_notified(self, key: str, value: int) -> int:
-        """Verify the given value is between 0 and 100
+    def _validate_percent_notified(self, key: str, value: int) -> int:
+        """Verify the given value is between 0 and 100 (inclusive)
+
+        Args:
+            key: Name of the column data is being entered into
+            value: The value to test
 
         Raises:
             ValueError: If the given value does not match required criteria
