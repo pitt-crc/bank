76c73f88e3ff1257b9753fd09144b716d4fc3bd6
Daniel <djperrefort@gmail.com>
2022-05-02 17:21:07 -0400

    Outlines tests for proposal services

---
+++ tests/account_logic/test_ProposalServices.py
@@ -0,0 +1,278 @@
+from datetime import timedelta
+from unittest import TestCase
+
+from sqlalchemy import select, join
+
+from bank import settings
+from bank.account_logic import ProposalServices
+from bank.exceptions import MissingProposalError, ProposalExistsError
+from bank.orm import Session, Proposal, Account, Allocation
+from tests._utils import ProposalSetup, EmptyAccountSetup
+
+joined_tables = join(join(Allocation, Proposal), Account)
+sus_query = select(Allocation.service_units) \
+    .select_from(joined_tables) \
+    .where(Account.name == settings.test_account) \
+    .where(Allocation.cluster_name == settings.test_cluster) \
+    .where(Proposal.is_active)
+
+active_proposal_query = select(Proposal) \
+    .join(Account) \
+    .where(Account.name == settings.test_account) \
+    .where(Proposal.is_active)
+
+
+class CreateProposal(EmptyAccountSetup, TestCase):
+    """Test the creation of proposals via the ``create_proposal`` method"""
+
+    def setUp(self) -> None:
+        """Delete any proposals that may already exist for the test account"""
+
+        super().setUp()
+        self.account = ProposalServices(settings.test_account)
+
+    def test_default_sus_are_zero(self) -> None:
+        """Test proposals are created with zero service units by default"""
+
+        self.account.create_proposal()
+        with Session() as session:
+            query = select(Proposal).join(Account).where(Account.name == settings.test_account)
+            proposal = session.execute(query).scalars().first()
+
+            self.assertTrue(proposal)
+            for alloc in proposal.allocations:
+                self.assertEqual(0, alloc.service_units)
+
+    def test_non_default_sus_are_set(self) -> None:
+        """Test proposals are assigned the number of sus specified by kwargs"""
+
+        self.account.create_proposal(**{settings.test_cluster: 1000})
+        with Session() as session:
+            service_units = session.execute(sus_query).scalars().first()
+            self.assertEqual(1000, service_units)
+
+    def test_error_if_already_exists(self) -> None:
+        """Test a ``ProposalExistsError`` error is raised if the proposal already exists"""
+
+        self.account.create_proposal()
+        with self.assertRaises(ProposalExistsError):
+            self.account.create_proposal()
+
+    def test_error_on_negative_sus(self) -> None:
+        """Test an error is raised when assigning negative service units"""
+
+        with self.assertRaises(ValueError):
+            self.account.create_proposal(**{settings.test_cluster: -1})
+
+
+class DeleteProposal(ProposalSetup, TestCase):
+    """Test the deletion of proposals via the ``delete_proposal`` method"""
+
+    def setUp(self) -> None:
+        """Delete any proposals that may already exist for the test account"""
+
+        super().setUp()
+        self.account = ProposalServices(settings.test_account)
+
+    def test_delete_active_proposal(self) -> None:
+        """Test the active proposal is deleted from the database"""
+
+        self.account.delete_proposal()
+        with Session() as session:
+            active_proposal = session.execute(active_proposal_query).scalars().first()
+            self.assertIsNone(active_proposal, 'Active proposal was not deleted')
+
+    def test_delete_proposal_by_id(self) -> None:
+        """Test a specific proposal is deleted when an id is given"""
+
+        delete_pid = 1
+        self.account.delete_proposal(pid=delete_pid)
+
+        with Session() as session:
+            query = select(Proposal).where(Proposal.id == delete_pid)
+            proposal = session.execute(query).first()
+            self.assertIsNone(proposal, f'Proposal {delete_pid} was not deleted')
+
+
+class ModifyProposal(ProposalSetup, TestCase):
+    """Test the modification of allocated sus via the ``set_cluster_allocation`` method"""
+
+    def setUp(self) -> None:
+        """Delete any proposals that may already exist for the test account"""
+
+        super().setUp()
+        self.account = ProposalServices(settings.test_account)
+
+    def test_sus_are_modified(self) -> None:
+        """Test sus are overwritten in the proposal"""
+
+        with Session() as session:
+            old_proposal = session.execute(active_proposal_query).scalars().first()
+            old_type = old_proposal.proposal_type
+            old_start = old_proposal.start_date
+            old_end = old_proposal.end_date
+
+        self.account.modify_proposal(**{settings.test_cluster: 12345})
+        with Session() as session:
+            new_proposal = session.execute(active_proposal_query).scalars().first()
+            new_sus = session.execute(sus_query).scalars().first()
+
+            # Check only service units are overwritten
+            self.assertEqual(12345, new_sus)
+            self.assertEqual(old_start, new_proposal.start_date)
+            self.assertEqual(old_end, new_proposal.end_date)
+            self.assertEqual(old_type, new_proposal.proposal_type)
+
+    def test_dates_are_modified(self) -> None:
+        """Test start and end dates are overwritten in the proposal"""
+
+        with Session() as session:
+            old_proposal = session.execute(active_proposal_query).scalars().first()
+            new_start_date = old_proposal.start_date + timedelta(days=500)
+            new_end_date = old_proposal.end_date + timedelta(days=800)
+            old_type = old_proposal.proposal_type
+
+        self.account.modify_proposal(start_date=new_start_date, end_date=new_end_date)
+        with Session() as session:
+            new_proposal = session.execute(active_proposal_query).scalars().first()
+            self.assertEqual(new_start_date, new_proposal.start_date)
+            self.assertEqual(new_end_date, new_proposal.end_date)
+            self.assertEqual(old_type, new_proposal.proposal_type)
+
+    def test_error_on_bad_cluster_name(self) -> None:
+        """Test a ``ValueError`` is raised if the cluster name is not defined in application settings"""
+
+        fake_cluster_name = 'fake_cluster'
+        with self.assertRaises(ValueError):
+            self.account.modify_proposal(**{fake_cluster_name: 1000})
+
+    def test_error_on_negative_sus(self) -> None:
+        """Test a ``ValueError`` is raised when assigning negative service units"""
+
+        with self.assertRaises(ValueError):
+            self.account.modify_proposal(**{settings.test_cluster: -1})
+
+
+class AddSus(ProposalSetup, TestCase):
+    """Test the addition of sus via the ``add`` method"""
+
+    def setUp(self) -> None:
+        """Delete any proposals that may already exist for the test account"""
+
+        super().setUp()
+        self.account = ProposalServices(settings.test_account)
+
+    def test_sus_are_added(self) -> None:
+        """Test SUs are added to the proposal"""
+
+        with Session() as session:
+            original_sus = session.execute(sus_query).scalars().first()
+
+        sus_to_add = 1000
+        self.account.add_sus(**{settings.test_cluster: sus_to_add})
+
+        with Session() as session:
+            new_sus = session.execute(sus_query).scalars().first()
+            self.assertEqual(original_sus + sus_to_add, new_sus)
+
+    def test_error_on_bad_cluster_name(self) -> None:
+        """Test a ``ValueError`` is raised if the cluster name is not defined in application settings"""
+
+        fake_cluster_name = 'fake_cluster'
+        with self.assertRaises(ValueError):
+            self.account.add_sus(**{fake_cluster_name: 1000})
+
+    def test_error_on_negative_sus(self) -> None:
+        """Test a ``ValueError`` is raised when assigning negative service units"""
+
+        with self.assertRaises(ValueError):
+            self.account.add_sus(**{settings.test_cluster: -1})
+
+
+class SubtractSus(ProposalSetup, TestCase):
+    """Test the subtraction of sus via the ``subtract`` method"""
+
+    def setUp(self) -> None:
+        """Delete any proposals that may already exist for the test account"""
+
+        super().setUp()
+        self.account = ProposalServices(settings.test_account)
+
+    def test_sus_are_subtracted(self) -> None:
+        """Test SUs are removed from the proposal"""
+
+        with Session() as session:
+            original_sus = session.execute(sus_query).scalars().first()
+
+        sus_to_subtract = 1000
+        self.account.subtract_sus(**{settings.test_cluster: sus_to_subtract})
+
+        with Session() as session:
+            new_sus = session.execute(sus_query).scalars().first()
+            self.assertEqual(original_sus - sus_to_subtract, new_sus)
+
+    def test_error_on_bad_cluster_name(self) -> None:
+        """Test a ``ValueError`` is raised if the cluster name is not defined in application settings"""
+
+        with self.assertRaises(ValueError):
+            self.account.subtract_sus(fake_cluster_name=1000)
+
+    def test_error_on_negative_sus(self) -> None:
+        """Test a ``ValueError`` is raised when assigning negative service units"""
+
+        with self.assertRaises(ValueError):
+            self.account.subtract_sus(**{settings.test_cluster: -1})
+
+    def test_error_on_over_subtraction(self) -> None:
+        """Test a value error is raised for subtraction resulting in negative sus"""
+
+        with self.assertRaises(ValueError):
+            self.account.subtract_sus(**{settings.test_cluster: self.num_proposal_sus + 100})
+
+
+class MissingProposalErrors(EmptyAccountSetup, TestCase):
+    """Tests for errors when manipulating an account that does not have a proposal"""
+
+    def setUp(self) -> None:
+        """Delete any proposals that may already exist for the test account"""
+
+        super().setUp()
+        self.account = ProposalServices(settings.test_account)
+
+    def test_error_on_delete(self) -> None:
+        """Test for a ``MissingProposalError`` error when deleting a missing proposal"""
+
+        with self.assertRaises(MissingProposalError):
+            self.account.delete_proposal()
+
+    def test_error_on_modify(self) -> None:
+        """Test a ``MissingProposalError`` error is raised when modifying a missing proposal"""
+
+        with self.assertRaises(MissingProposalError):
+            ProposalServices(settings.test_account).modify_proposal(**{settings.test_cluster: 1, 'pid': 1000})
+
+    def test_error_on_add(self) -> None:
+        """Test a ``MissingProposalError`` error is raised when adding to a missing proposal"""
+
+        with self.assertRaises(MissingProposalError):
+            self.account.add_sus(**{settings.test_cluster: 1})
+
+    def test_error_on_subtract(self) -> None:
+        """Test a ``MissingProposalError`` error is raised when subtracting from a missing proposal"""
+
+        with self.assertRaises(MissingProposalError):
+            self.account.subtract_sus(**{settings.test_cluster: 1})
+
+
+class PreventOverlappingProposals(EmptyAccountSetup, TestCase):
+    """Tests to ensure proposals cannot overlap in time"""
+
+    def test_error_on_proposal_creation(self):
+        """Test new proposals are not allowed to overlap with existing proposals"""
+
+        raise NotImplementedError
+
+    def test_error_on_proposal_modification(self):
+        """Test existing proposals can not be modified to overlap with other proposals"""
+
+        raise NotImplementedError
