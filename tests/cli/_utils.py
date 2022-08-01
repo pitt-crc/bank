bbf3975991db398e93342d3ed47e572a1fd0a7ec
Daniel <djperrefort@gmail.com>
2022-08-01 09:23:37 -0400

    Update tests for proposal parser

--- tests/cli/_utils.py
+++ tests/cli/_utils.py
@@ -1,4 +1,4 @@
-"""Generic utilities and mixin classes used when testing the ``cli`` module"""
+"""Generic utilities and mixin classes for testing commandline functionality"""
 
 import inspect
 
@@ -6,13 +6,13 @@ import inspect
 class CLIAsserts:
     """Mixin class that provides custom assert methods for testing command line parsing"""
 
-    def assert_parser_matches_func_signature(self, cmd: str) -> None:
+    def assert_parser_matches_func_signature(self, parser, cmd: str) -> None:
         """Assert the arguments retrieved from a parsed string match the
         signature of the function they are passed to
         """
 
         cmd = cmd.split()
-        known_args, unknown_args = self.parser.parse_known_args(cmd)
+        known_args, unknown_args = parser.parse_known_args(cmd)
         if unknown_args:
             self.fail(f'Parser received unknown arguments: {unknown_args}')
 
