e6f7d97ae8179f91803f8465d9d730af03faf0f8
Daniel <djperrefort@gmail.com>
2022-08-02 09:44:36 -0400

    Add metadata to package init

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def get_meta():
         - license
     """
 
-    init_path = Path(__file__).resolve().parent / 'apps/__init__.py'
+    init_path = Path(__file__).resolve().parent / 'bank/__init__.py'
     init_text = init_path.read_text()
 
     version_regex = re.compile("__version__ = '(.*?)'")
