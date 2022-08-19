fd4e0d336737c1e761377fcbec20dc4a96ad9c50
Daniel <djperrefort@gmail.com>
2022-08-19 10:40:30 -0400

    Update metadata in setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,16 @@ from pathlib import Path
 
 from setuptools import find_packages, setup
 
-PACKAGE_REQUIREMENTS = Path(__file__).parent / 'requirements.txt'
-DOCUMENTATION_REQUIREMENTS = Path(__file__).parent / 'docs' / 'requirements.txt'
+CURRENT_DIR = Path(__file__).resolve().parent
+PACKAGE_REQUIREMENTS = CURRENT_DIR / 'requirements.txt'
+DOCUMENTATION_REQUIREMENTS = CURRENT_DIR / 'docs' / 'requirements.txt'
+INIT_PATH = CURRENT_DIR / 'bank' / '__init__.py'
+README_PATH = CURRENT_DIR / 'README.md'
 
 
-def get_long_description():
+def get_long_description(readme_file=README_PATH):
     """Return a long description of tha parent package"""
 
-    readme_file = Path(__file__).parent / 'README.md'
     return readme_file.read_text()
 
 
@@ -23,14 +25,9 @@ def get_requirements(path):
         return req_file.read().splitlines()
 
 
-def get_meta():
-    """Return package metadata including the:
-        - author
-        - version
-        - license
-    """
+def get_meta(init_path=INIT_PATH):
+    """Return package author, version, and license from the init file"""
 
-    init_path = Path(__file__).resolve().parent / 'bank/__init__.py'
     init_text = init_path.read_text()
 
     version_regex = re.compile("__version__ = '(.*?)'")
@@ -39,13 +36,13 @@ def get_meta():
     author_regex = re.compile("__author__ = '(.*?)'")
     author = author_regex.findall(init_text)[0]
 
-    # license_regex = re.compile("__license__ = '(.*?)'")
-    # license_type = license_regex.findall(init_text)[0]
+    license_regex = re.compile("__license__ = '(.*?)'")
+    license_type = license_regex.findall(init_text)[0]
 
-    return author, version
+    return author, version, license_type
 
 
-_author, _version = get_meta()
+_author, _version, _license_type = get_meta()
 setup(
     name='crc-bank',
     description='Banking application for resource allocation in Slurm based HPC systems.',
@@ -62,11 +59,18 @@ setup(
         'tests': ['coverage'],
     },
     author=_author,
-    keywords='Pitt, CRC, HPC',
+    maintainer=_author,
+    keywords='pitt,crc,hpc,banking,slurm',
     long_description=get_long_description(),
     long_description_content_type='text/markdown',
-    # license=_license_type,
+    license=_license_type,
     classifiers=[
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+        'Topic :: System :: Systems Administration'
     ]
 )
