0b78ec0d0206931f4f94e58ed812a650ddce92f4
Daniel <djperrefort@gmail.com>
2022-09-20 01:50:18 -0400

    Update test account name

--- bank/settings.py
+++ bank/settings.py
@@ -89,7 +89,7 @@ from environ import environ
 _CUR_DIR = Path(__file__).resolve().parent
 
 # Settings for running the test suite.
-test_account = 'sam'
+test_account = 'account1'
 test_cluster = 'smp'
 
 # Define how dates should be displayed as strings (in errors, emails, and STDOUT messages)
