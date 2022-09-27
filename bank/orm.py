df33eb6b3b072cf716be15a45b0d64744485a2fe
Nickolas Comeau <nickolas.comeau@gmail.com>
2022-09-27 12:13:24 -0400

    Keep track of service units used in each allocation

--- bank/orm.py
+++ bank/orm.py
@@ -149,11 +149,12 @@ class Allocation(Base):
     has run out.
 
     Table Fields:
-      - id             (Integer): Primary key for this table
-      - proposal_id (ForeignKey): Primary key for the ``proposal`` table
-      - cluster_name    (String): Name of the allocated cluster
-      - service_units  (Integer): Number of allocated service units
-      - final_usage    (Integer): Total service units utilized at proposal expiration
+      - id                   (Integer): Primary key for this table
+      - proposal_id          (ForeignKey): Primary key for the ``proposal`` table
+      - cluster_name         (String): Name of the allocated cluster
+      - service_units_total  (Integer): Number of allocated service units
+      - service_units_used   (Integer): Number of used service units
+      - final_usage          (Integer): Total service units utilized at proposal expiration
 
     Relationships:
       - proposal (Proposal): Many to one
@@ -164,7 +165,8 @@ class Allocation(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     proposal_id = Column(Integer, ForeignKey(Proposal.id))
     cluster_name = Column(String, nullable=False)
-    service_units = Column(Integer, nullable=False)
+    service_units_total = Column(Integer, nullable=False)
+    service_units_used = Column(Integer, nullable=False)
     final_usage = Column(Integer, nullable=True)
 
     proposal = relationship('Proposal', back_populates='allocations')
