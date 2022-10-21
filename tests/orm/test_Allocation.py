2c3d038abe9be5c62eb99ca2047e9e71179b868b
Daniel <djperrefort@gmail.com>
2022-10-21 13:35:12 -0400

    Expand tests for used service unit validation

--- tests/orm/test_Allocation.py
+++ tests/orm/test_Allocation.py
@@ -1,22 +1,18 @@
+"""Tests for the ``Allocation`` class"""
+
 from unittest import TestCase
 
 from bank import settings
 from bank.orm import Allocation
 
 
-class ServiceUnitsValidation(TestCase):
-    """Tests for the validation of the ``service_units`` column"""
+class TotalServiceUnitsValidation(TestCase):
+    """Tests for the validation of the ``service_units_total`` column"""
 
-    def test_negative_service_units_total(self) -> None:
+    def test_negative_service_units(self) -> None:
         """Test for a ``ValueError`` when the total number of service units is negative"""
 
-        with self.assertRaises(ValueError):
-            Allocation(service_units_total=-1)
-
-    def test_negative_service_units_used(self) -> None:
-        """Test for a ``ValueError`` when the number of service units used are negative"""
-
-        with self.assertRaises(ValueError):
+        with self.assertRaisesRegex(ValueError, 'must be a non-negative integer'):
             Allocation(service_units_total=-1)
 
     def test_value_is_assigned(self) -> None:
@@ -25,6 +21,30 @@ class ServiceUnitsValidation(TestCase):
         for num_sus in (0, 10):
             allocation = Allocation(service_units_total=num_sus)
             self.assertEqual(num_sus, allocation.service_units_total)
+
+
+class UsedServiceUnitsValidation(TestCase):
+    """Tests for the validation of the ``service_units_used`` column"""
+
+    def test_negative_service_units(self) -> None:
+        """Test for a ``ValueError`` when the total number of service units is negative"""
+
+        with self.assertRaisesRegex(ValueError, 'must be a non-negative integer'):
+            Allocation(service_units_used=-1)
+
+    def test_value_is_assigned(self) -> None:
+        """Test the validated value is assigned to the table instance"""
+
+        for num_sus in (0, 10):
+            allocation = Allocation(service_units_used=num_sus)
+            self.assertEqual(num_sus, allocation.service_units_used)
+
+    def test_can_exceed_total(self) -> None:
+        """Test the used service units are allowed to exceed the total allocated service units"""
+
+        allocation = Allocation(service_units_total=100, service_units_used=105)
+        self.assertEqual(100, allocation.service_units_total)
+        self.assertEqual(105, allocation.service_units_used)
 
 
 class ClusterNameValidation(TestCase):
