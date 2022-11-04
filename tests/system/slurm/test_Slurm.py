79aff70c0dde61b4db6c7fae8f4739d4fea69b4b
Daniel <djperrefort@gmail.com>
2022-11-04 14:57:18 -0400

    Add unpatched version of tests for Slurm.is_installed

--- tests/system/slurm/test_Slurm.py
+++ tests/system/slurm/test_Slurm.py
@@ -20,13 +20,18 @@ class IsInstalled(TestCase):
     """Tests for the ``is_installed`` method"""
 
     @patch('bank.system.shell.ShellCmd._subprocess_call', return_value=('slurm 0.0.0', ''))
-    def test_slurm_is_installed(self, *args) -> None:
-        """Test slurm registers as being installed in the test environment"""
+    def test_is_installed_true(self, *args) -> None:
+        """Test the return value is ``True`` when slurm is installed"""
 
         self.assertTrue(Slurm.is_installed())
 
     @patch('bank.system.shell.ShellCmd._subprocess_call', return_value=('', 'command not found'))
-    def test_slurm_not_installed(self, *args) -> None:
-        """Test slurm registers as being installed in the test environment"""
+    def test_not_installed_false(self, *args) -> None:
+        """Test the return value is ``False`` when slurm is not installed"""
 
         self.assertFalse(Slurm.is_installed())
+
+    def test_installed_in_test_env(self) -> None:
+        """Test slurm is installed in the test environment"""
+
+        self.assertTrue(Slurm.is_installed())
