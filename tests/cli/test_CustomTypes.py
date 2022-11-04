19b0ac15db26f5a94d828745bb6599f95821e893
Daniel <djperrefort@gmail.com>
2022-11-04 16:10:45 -0400

    Adds tests for date type casting

--- tests/cli/test_CustomTypes.py
+++ tests/cli/test_CustomTypes.py
@@ -1,9 +1,39 @@
 """Tests for the ``ArgumentTypes`` class"""
 
 from argparse import ArgumentTypeError
+from datetime import date
 from unittest import TestCase
 
+from bank import settings
 from bank.cli import ArgumentTypes
+
+
+class DateCasting(TestCase):
+    """Tests for type casting via the ``date`` method"""
+
+    def test_blank_string_error(self) -> None:
+        """Test an ``ArgumentTypeError`` is raised for a blank string"""
+
+        with self.assertRaises(ArgumentTypeError):
+            ArgumentTypes.date('')
+
+    def test_invalid_value_err(self) -> None:
+        """Test an ``ArgumentTypeError`` is raised for strings not representing valid dates"""
+
+        with self.assertRaises(ArgumentTypeError):
+            ArgumentTypes.date('this is not a date')
+
+    def test_invalid_format_err(self) -> None:
+        self.assertNotEqual('%b %d %Y', settings.date_format)
+        with self.assertRaises(ArgumentTypeError):
+            ArgumentTypes.date('Nov 07 1993')
+
+    def test_valid_format(self) -> None:
+        """Test date strings matching the format in application settings are returned as date objects"""
+
+        test_date = date(2000, 11, 12)
+        test_date_str = test_date.strftime(settings.date_format)
+        self.assertEqual(test_date, ArgumentTypes.date(test_date_str))
 
 
 class NonNegativeIntCasting(TestCase):
