689eb33f5b9db1a2f5dec3c6638f56bfd3d36964
Nickolas Comeau <nickolas.comeau@gmail.com>
2022-12-01 16:07:49 -0500

    AdminServices tests

--- tests/account_logic/test_AdminServices.py
+++ tests/account_logic/test_AdminServices.py
@@ -3,14 +3,9 @@ from unittest import TestCase
 from sqlalchemy import select
 
 from bank import settings
-from bank.account_logic import AccountServices, AdminServices
-from bank.orm import Account, DBConnection, Proposal
+from bank.account_logic import AdminServices
+from bank.orm import Account, DBConnection
 from bank.system.slurm import SlurmAccount
-from tests._utils import InvestmentSetup, ProposalSetup
-
-active_proposal_query = select(Proposal).join(Account) \
-    .where(Account.name == settings.test_account) \
-    .where(Proposal.is_active)
 
 
 class FindUnlockedAccounts(TestCase):
@@ -18,26 +13,43 @@ class FindUnlockedAccounts(TestCase):
 
     def setUp(self) -> None:
         """Instantiate a SlurmAccount and AdminServices objects for finding account tests"""
-        super().setUp()
-        self.slurm_account = SlurmAccount(settings.test_account)
+
+        self.slurm_account1 = SlurmAccount(settings.test_account)
+        self.slurm_account2 = SlurmAccount('account2')
         self.admin_services = AdminServices()
 
-    def test_unlocked_account_found(self) -> None:
-        """Test that an unlocked account is found"""
+    def test_unlocked_accounts_found(self) -> None:
+        """Test that an unlocked accounts are found"""
 
-        # Unlock the account
-        self.slurm_account.set_locked_state(False, settings.test_cluster)
+        # Unlock multiple accounts
+        self.slurm_account1.set_locked_state(False, settings.test_cluster)
+        self.slurm_account2.set_locked_state(False, settings.test_cluster)
 
-        # The account should be in the list of unlocked accounts
+        # The accounts should be in the list of unlocked accounts
         unlocked_accounts_by_cluster = self.admin_services.find_unlocked_account_names()
-        self.assertIn(self.slurm_account.account_name, unlocked_accounts_by_cluster[settings.test_cluster])
+        self.assertIn(self.slurm_account1.account_name, unlocked_accounts_by_cluster[settings.test_cluster])
+        self.assertIn(self.slurm_account2.account_name, unlocked_accounts_by_cluster[settings.test_cluster])
 
-    def test_locked_account_not_found(self) -> None:
-        """Test that a locked account is not found"""
+    def test_locked_accounts_not_found(self) -> None:
+        """Test that locked accounts are not found"""
 
-        # Lock the account
-        self.slurm_account.set_locked_state(True, settings.test_cluster)
+        # Lock multiple accounts
+        self.slurm_account1.set_locked_state(True, settings.test_cluster)
+        self.slurm_account2.set_locked_state(True, settings.test_cluster)
 
-        # The account should not be in the list of unlocked accounts
+        # The accounts should not be in the list of unlocked accounts
         unlocked_accounts_by_cluster = self.admin_services.find_unlocked_account_names()
-        self.assertNotIn(self.slurm_account.account_name, unlocked_accounts_by_cluster[settings.test_cluster])
+        self.assertNotIn(self.slurm_account1.account_name, unlocked_accounts_by_cluster[settings.test_cluster])
+        self.assertNotIn(self.slurm_account2.account_name, unlocked_accounts_by_cluster[settings.test_cluster])
+
+    def test_unlocked_accounts_only_found(self) -> None:
+        """Test that, amongst accounts that are locked, only unlocked accounts are found"""
+
+        # Lock one account and unlock another
+        self.slurm_account1.set_locked_state(True, settings.test_cluster)
+        self.slurm_account2.set_locked_state(False, settings.test_cluster)
+
+        # The unlocked account should be in the list of unlocked accounts, while the locked account should not
+        unlocked_accounts_by_cluster = self.admin_services.find_unlocked_account_names()
+        self.assertNotIn(self.slurm_account1.account_name, unlocked_accounts_by_cluster[settings.test_cluster])
+        self.assertIn(self.slurm_account2.account_name, unlocked_accounts_by_cluster[settings.test_cluster])
