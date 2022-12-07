53ea452452538c70eb579508f2c66e270e9bca2b
Daniel <djperrefort@gmail.com>
2022-12-07 16:05:53 -0500

    Breaks admin parser tests up by subparser

--- tests/cli/test_AdminParser.py
+++ tests/cli/test_AdminParser.py
@@ -1,33 +1,56 @@
 """Tests for the ``AdminParser`` class"""
-
-from unittest import TestCase, skip
+from argparse import ArgumentError
+from unittest import TestCase
 
 from bank.cli import AdminParser
-from tests._utils import ProposalSetup
+from bank.settings import test_cluster
 from tests.cli._utils import CLIAsserts
 
 
-class SignatureMatchesCLI(ProposalSetup, CLIAsserts, TestCase):
-    """Test parser arguments match the signatures of the corresponding executable"""
+class UpdateStatus(CLIAsserts, TestCase):
+    """Test the ``update_status`` subparser"""
 
-    def setUp(self) -> None:
-        """Define an instance of the parser object being tested"""
+    def test_no_arguments(self) -> None:
+        """Test the subparser call is valid without any additional arguments"""
 
-        self.parser = AdminParser()
+        self.assert_parser_matches_func_signature(AdminParser(), 'update_status')
 
-    def test_update_status(self) -> None:
-        """Test the parsing of arguments by the ``update_status`` command"""
 
-        self.assert_parser_matches_func_signature(self.parser, 'update_status')
+class ListLocked(CLIAsserts, TestCase):
+    """Test the ``list_locked`` subparser"""
 
-    @skip('Functionality not implemented')
-    def test_list_locked(self) -> None:
-        """Test the parsing of arguments by the ``list_locked`` command"""
+    def test_no_arguments(self) -> None:
+        """Test the subparser call is valid without any additional arguments"""
 
-        self.assert_parser_matches_func_signature(self.parser, 'list_locked')
+        self.assert_parser_matches_func_signature(AdminParser(), 'list_locked')
 
-    @skip('Functionality not implemented')
-    def test_list_unlocked(self) -> None:
-        """Test the parsing of arguments by the ``list_unlocked`` command"""
+    def test_single_clusters_argument(self) -> None:
+        """Test the ``--clusters`` argument accepts at least one cluster name"""
 
-        self.assert_parser_matches_func_signature(self.parser, 'list_unlocked')
+        self.assert_parser_matches_func_signature(AdminParser(), f'list_locked --clusters {test_cluster}')
+
+    def test_error_invalid_cluster(self) -> None:
+        """Test ``--clusters`` arguments are not valid unless defined in application settings"""
+
+        with self.assertRaises(ArgumentError):
+            self.assert_parser_matches_func_signature(AdminParser(), 'list_locked --clusters fake_cluster')
+
+
+class ListUnlocked(CLIAsserts, TestCase):
+    """Test the ``list_unlocked`` subparser"""
+
+    def test_no_arguments(self) -> None:
+        """Test the subparser call is valid without any additional arguments"""
+
+        self.assert_parser_matches_func_signature(AdminParser(), 'list_unlocked')
+
+    def test_single_clusters_argument(self) -> None:
+        """Test the ``--clusters`` argument accepts at least one cluster name"""
+
+        self.assert_parser_matches_func_signature(AdminParser(), 'list_unlocked --clusters development')
+
+    def test_error_invalid_cluster(self) -> None:
+        """Test ``--clusters`` arguments are not valid unless defined in application settings"""
+
+        with self.assertRaises(ArgumentError):
+            self.assert_parser_matches_func_signature(AdminParser(), 'list_unlocked --clusters fake_cluster')
