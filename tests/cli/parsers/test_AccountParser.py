ed5377bc126ca05ed3f9e23e14869fd1143bada7
Daniel <djperrefort@gmail.com>
2023-01-05 14:33:20 -0500

    Breaks AccountParser tests into seperate methods

--- tests/cli/parsers/test_AccountParser.py
+++ tests/cli/parsers/test_AccountParser.py
@@ -8,39 +8,40 @@ from tests._utils import ProposalSetup
 from tests.cli.parsers._utils import CLIAsserts
 
 
-class SignatureMatchesCLI(ProposalSetup, CLIAsserts, TestCase):
-    """Test parser arguments match the signatures of the corresponding executable"""
-
-    def setUp(self) -> None:
-        """Define an instance of the parser object being tested"""
-
-        self.parser = AccountParser()
+class Info(ProposalSetup, CLIAsserts, TestCase):
+    """Test the ``info`` subparser"""
 
     def test_account_info(self) -> None:
         """Test the parsing of arguments by the ``info`` command"""
 
-        self.assert_parser_matches_func_signature(self.parser, f'info {settings.test_accounts[0]}')
+        self.assert_parser_matches_func_signature(AccountParser(), f'info {settings.test_accounts[0]}')
 
-    def test_lock_list_clusters(self) -> None:
-        """Test argument parsing for the ``lock`` command`"""
 
-        # Lock on a specific cluster
+class Lock(ProposalSetup, CLIAsserts, TestCase):
+    """Test the ``lock`` subparser"""
+
+    def test_lock_all_clusters(self) -> None:
+        """Test argument parsing for the ``lock`` command with ``--all-clusters`` specified"""
+
+        self.assert_parser_matches_func_signature(AccountParser(), f'lock {settings.test_accounts[0]} --all-clusters')
+
+    def test_lock_single_cluster(self) -> None:
+        """Test argument parsing for the ``lock`` command for a single cluster name"""
+
         self.assert_parser_matches_func_signature(
-            self.parser,
-            f'lock {settings.test_accounts[0]} --cluster {settings.test_cluster}'
-        )
+            AccountParser(), f'lock {settings.test_accounts[0]} --cluster {settings.test_cluster}')
 
-        # Lock on all clusters
-        self.assert_parser_matches_func_signature(self.parser, f'lock {settings.test_accounts[0]} --all_clusters')
 
-    def test_unlock_clusters(self) -> None:
-        """Test argument parsing for the ``unlock`` command """
+class Unlock(ProposalSetup, CLIAsserts, TestCase):
+    """Test the ``unlock`` subparser"""
 
-        # Unlock on a specific cluster
+    def test_unlock_single_cluster(self) -> None:
+        """Test argument parsing for the ``unlock`` command for a single cluster name"""
+
+        self.assert_parser_matches_func_signature(AccountParser(), f'unlock {settings.test_accounts[0]} --all-clusters')
+
+    def test_unlock_all_clusters(self) -> None:
+        """Test argument parsing for the ``unlock`` command with ``--all-clusters`` specified"""
+
         self.assert_parser_matches_func_signature(
-            self.parser,
-            f'unlock {settings.test_accounts[0]} --cluster {settings.test_cluster}'
-        )
-
-        # Unlock on a specific cluster
-        self.assert_parser_matches_func_signature(self.parser, f'unlock {settings.test_accounts[0]} --all_clusters')
+            AccountParser(), f'unlock {settings.test_accounts[0]} --cluster {settings.test_cluster}')
