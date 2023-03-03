1b1b4947360f878b0bd2eecf76eea5a2d44ae892
Nickolas Comeau <nickolas.comeau@pitt.edu>
2023-03-03 17:35:32 -0500

    Add comment

--- crc_bank.py
+++ crc_bank.py
@@ -601,6 +601,7 @@ elif args["renewal"]:
             investor_rows = investor_table.find(account=args["<account>"])
             for investor_row in investor_rows:
                 if need_to_rollover > 0:
+                    # Divide by 1 if years left is less than 1 to prevent division by 0
                     to_withdraw = (
                         investor_row[f"service_units"] - investor_row[f"withdrawn_sus"]
                     ) // (utils.years_left(investor_row["end_date"]) or 1)
