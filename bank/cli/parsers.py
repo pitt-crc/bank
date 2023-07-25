1d1f57dc5a05c2915f1ce23cf35ec1423d6a801c
Comeani <nlc60@pitt.edu>
2023-07-25 14:19:27 -0700

    Add CLI option for Account insert

--- bank/cli/parsers.py
+++ bank/cli/parsers.py
@@ -178,6 +178,11 @@ class AccountParser(BaseParser):
         info_parser.set_defaults(function=AccountServices.info)
         info_parser.add_argument(**account_argument)
 
+        insert_parser = parent_parser.add_parser('insert', help='first time insertion of an entry into the database '
+                                                                'for the account')
+        insert_parser.set_defaults(function=AccountServices.insert)
+        insert_parser.add_argument(**account_argument)
+
 
 class ProposalParser(BaseParser):
     """Commandline interface for managing individual proposals
