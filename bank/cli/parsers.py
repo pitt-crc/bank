150041a628a7639cac08c441f96573d2786421af
Nickolas Comeau <nlc60@pitt.edu>
2023-07-27 11:16:13 -0700

    Preferentially use settings, defaulting to what slurm provides

--- bank/cli/parsers.py
+++ bank/cli/parsers.py
@@ -244,6 +244,8 @@ class ProposalParser(BaseParser):
         parser.add_argument('--all-clusters', **su_argument, help='service units awarded across all clusters')
 
         # Add per-cluster arguments for setting service units
+        clusters = settings.clusters or Slurm.cluster_names()        
+
         for cluster in settings.clusters:
             parser.add_argument(f'--{cluster}', **su_argument, help=f'service units awarded on the {cluster} cluster')
 
