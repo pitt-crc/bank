f52af0cafeb2652233d120eb0834f045173f66dc
Nickolas Comeau <nlc60@pitt.edu>
2023-08-29 13:49:02 -0400

    Deploy fix (#317)
    * fix initializing of floating_su_total
    
    * Update settings to use production clusters and production bank db/log location
    
    * Create folder for bank DB to initialize in
    
    * Remove bank db folder creation
    
    Don't include a step to create folder for db to exist in, we're going to manage this with a configurable setting file.
    
    * Include bank location setup in documentation build
    
    ---------
    
    Co-authored-by: Nickolas Comeau <nickolas.comeau@pitt.edu>

--- bank/settings.py
+++ bank/settings.py
@@ -56,7 +56,7 @@ module:
    >>>
    >>> # The format used by the application to represent dates as strings
    >>> print(settings.date_format)
-   %m/%d/%y
+   %m/%d/%Y
 
 Application settings are cached at import and should not be modified during
 the application runtime. Likewise, modifications to environmental variables
@@ -68,7 +68,7 @@ during execution will not be recognized by the application.
    >>> # does not affect the application settings
    >>> os.environ['BANK_DATE_FORMAT'] = '%m-%d'
    >>> print(settings.date_format)
-   %m/%d/%y
+   %m/%d/%Y
 """
 
 from __future__ import annotations
@@ -84,18 +84,18 @@ test_cluster = 'development'
 nonexistent_account = 'fake_account'
 
 # Define how dates should be displayed as strings (in errors, emails, and STDOUT messages)
-date_format = '%m/%d/%y'
+date_format = '%m/%d/%Y'
 
 # Where and how to write log files
-log_path = _CUR_DIR / 'crc_bank.log'
+log_path = "/ihome/crc/bank/crc_bank.log"
 log_format = '[%(levelname)s] %(asctime)s - %(name)s - %(message)s'
 log_level = 'INFO'
 
 # Path to the application database
-db_path = f"sqlite:///{_CUR_DIR / 'crc_bank.db'}"
+db_path = "sqlite:////ihome/crc/bank/crc_bank.db"
 
 # A list of cluster names to track usage on
-clusters = ('development', 'all_clusters',)
+clusters = ('smp', 'mpi', 'htc', 'gpu', 'teach', 'invest')
 
 # Fraction of service units to carry over when rolling over investments
 # Should be a float between 0 and 1
